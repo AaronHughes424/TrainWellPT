58a92874f9e95b72465a1f8ed11a45d0f69b43b0
AaronHughes424 <aaronhughes424@gmail.com>
2022-06-21 11:31:08 +0000

    added stripe elements for payment

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51LD4zYC2liIoTkb4gVNqeM3ftcmm1qC98DoggIDg7Li6c4IAwGWLjp7XzmYSMVE2xxxUtDKKnt51HFzWBZYQ2EAr00RkWnPs6R',
+        'client_secret': 'test client secret'
     }
 
     return render(request, template, context)
