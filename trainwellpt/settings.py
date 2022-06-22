44ddb6e91219bc29247ed9203e242581598e17b9
AaronHughes424 <aaronhughes424@gmail.com>
2022-06-22 16:57:20 +0000

    added account email fix

--- trainwellpt/settings.py
+++ trainwellpt/settings.py
@@ -191,3 +191,5 @@ DEFAULT_FROM_EMAIL = 'TrainWellPT@example.com'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+ACCOUNT_EMAIL_VERIFICATION = 'none'
