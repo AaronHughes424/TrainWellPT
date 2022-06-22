d85e1edb7ea696da94af5ea53666c245a3031822
AaronHughes424 <aaronhughes424@gmail.com>
2022-06-22 17:06:45 +0000

    added email verification none to settings

--- trainwellpt/settings.py
+++ trainwellpt/settings.py
@@ -124,15 +124,16 @@ WSGI_APPLICATION = 'trainwellpt.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 if 'DATABASE_URL' in os.environ:
-    DATABASES  = {
+    DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
