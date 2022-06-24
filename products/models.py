a25b21228982cab28a14e35cb5efd97534d47080
AaronHughes424 <aaronhughes424@gmail.com>
2022-06-24 00:49:37 +0000

    updated review url/view/form

--- products/models.py
+++ products/models.py
@@ -32,15 +32,7 @@ class Product(models.Model):
 
 
 class ProductReview(models.Model):
-    product = models.ForeignKey(
-        Product, on_delete=models.CASCADE,
-        null=False, related_name='product')
-    review_content = models.TextField(
-        null=False, blank=False, max_length=254)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=False, related_name='product')
+    review_content = models.TextField(null=False, blank=False, max_length=254)
     author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
-    date_added = models.DateField(
-        auto_now_add=True, null=False, blank=False, editable=False
-    )
-
-    def __str__(self):
-        return self.name
+    date_added = models.DateField(auto_now_add=True, null=False, blank=False, editable=False)
