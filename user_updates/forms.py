c61523de9d4a723c5f23a04a148477448af81981
AaronHughes424 <aaronhughes424@gmail.com>
2022-06-25 16:35:23 +0000

    setting up user updates

--- user_updates/forms.py
+++ user_updates/forms.py
@@ -1,5 +1,4 @@
 from django import forms
-from .models import Diary
 
 
 # class DiaryForm(forms.ModelForm):
--- user_updates/models.py
+++ user_updates/models.py
@@ -1,24 +1,3 @@
 from django.db import models
-from django.contrib.auth.models import User
 
 # Create your models here.
-
-
-# class Diary(models.Model):
-#     """
-#     Diary Model
-#     """
-
-#     class Meta:
-#         ordering = ['-date_added']
-
-#     user = models.ForeignKey(User,
-#                              null=True,
-#                              blank=True,
-#                              on_delete=models.CASCADE)
-#     title = models.CharField(max_length=254)
-#     content = models.TextField()
-#     date_added = models.DateTimeField(auto_now_add=True)
-
-#     def __str__(self):
-#         return self.title
