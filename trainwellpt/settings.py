fa54129edaa7ba6a638c3a6faf7c2276ac1948af
AaronHughes424 <aaronhughes424@gmail.com>
2022-06-26 11:57:48 +0000

    updated emial settings to fix bug

--- trainwellpt/settings.py
+++ trainwellpt/settings.py
@@ -213,7 +213,7 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
-    EMAIL_BACKEND = 'django.core.mail.backend.console.EmailBackend'
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
     DEFAULT_FROM_EMAIL = 'TrainWellPT@example.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
